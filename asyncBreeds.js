59098a97792a2d3bc249a25c60357af33efea18b
JettScythe <jettscythe@hotmail.com>
2021-05-04 20:35:56 -0300

    completed with tests & async

---
+++ asyncBreeds.js
@@ -0,0 +1,16 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, cb) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) {
+      cb(data)
+    } else {
+      cb(undefined);
+    }
+  });
+};
+
+module.exports = breedDetailsFromFile;
